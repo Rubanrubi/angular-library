d7a4d6bf13afbbd978a545c358a3c120811a8d66
hariprakash-sparkout <hariprakash@sparkouttech.com>
2024-02-13 15:51:09 +0530

    init

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'angular-library' title`, () => {
+  it(`should have the 'test' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-library');
+    expect(app.title).toEqual('test');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-library app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('test app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,29 @@
-import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { SignaturePadComponent } from './component/shared/signature-pad/signature-pad.component';
+import { NgxGpAutocompleteDirective, NgxGpAutocompleteModule } from "@angular-magic/ngx-gp-autocomplete";
+import { Loader } from '@googlemaps/js-api-loader';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, SignaturePadComponent],
+  imports: [CommonModule, RouterOutlet,NgxGpAutocompleteModule],
+  providers:[{
+    provide: Loader,
+    useValue: new Loader({
+      apiKey: 'AIzaSyAFaylOBsuhYPYw9YqWmhN370xTvc6DXYU',
+      libraries: ['places']
+    })
+  },],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-library';
+  options:any
+
+  @ViewChild('ngxPlaces') placesRef!: NgxGpAutocompleteDirective;
+
+  public handleAddressChange(place: google.maps.places.PlaceResult) {
+    // Do some stuff
+  }
 }
