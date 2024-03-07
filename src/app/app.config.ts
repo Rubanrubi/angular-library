7ce4c657d35eb67919a609d02f8a1bc89be5eb5a
Rubanrubi <ruban@sparkouttech.com>
2024-03-07 12:13:08 +0530

    compodoc implementation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,8 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
+import { APP_ROUTES } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes) ]
+  providers: [provideRouter(APP_ROUTES) ]
 };
