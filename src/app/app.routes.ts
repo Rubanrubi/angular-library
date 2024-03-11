36a28e36d55c0614193b58cb1fb71e3a576c06b9
Rubanrubi <ruban@sparkouttech.com>
2024-03-12 00:15:12 +0530

    workflow code review

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 import { SignaturePadComponent } from './component/shared/signature-pad/signature-pad.component';
 
 export const APP_ROUTES: Routes = [
-  {path: '/testing', component: SignaturePadComponent },
+  { path: '/testing', component: SignaturePadComponent },
 ];
